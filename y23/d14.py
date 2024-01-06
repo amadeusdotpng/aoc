794ebb9f062a23eff2e9f1a66ba4e8cefbab454a
Amadeus8182 <amadeus8182@gmail.com>
2024-01-06 04:51:28 -0600

    2023 day 14 refactor

--- y23/d14.py
+++ y23/d14.py
@@ -1,24 +1,15 @@
 from functools import cache
 from typing import List
 
-def cycle(map: str) -> str:
-    map = tiltMap(map)
-    map = tiltMap(map)
-    map = tiltMap(map)
-    return tiltMap(map)
-
-def tiltMap(map: str) -> str:
-    map = map.splitlines()
-    out = []
-    for i in range(len(map[0])):
-        col = ''
-        for row in map:
-            col += row[i]
-        out.append(tiltCol(col))
-    return '\n'.join(out)
-
 def tiltCol(col: str) -> str:
     return '#'.join('O'*S.count('O')+S.replace('O', '') for S in col.split('#'))[::-1]
+
+def transpose(map: str, tilt: bool) -> str:
+    L = [tiltCol(''.join(s)) if tilt else ''.join(s)[::-1] for s in zip(*map.splitlines())]
+    return '\n'.join(L)
+
+def getLoadCol(col: str) -> int:
+    return sum(i+1 for i,c in enumerate(col) if c == 'O')
 
 def getLoadMap(map: str):
     map = map.splitlines()
@@ -30,24 +21,17 @@ def getLoadMap(map: str):
         sum += getLoadCol(col[::-1])
     return sum
 
-def getLoadCol(col: str) -> int:
-    return sum(i+1 for i,c in enumerate(col) if c == 'O')
-
-def transpose(map: str) -> str:
-    map = map.splitlines()
-    L = []
-    for i in range(len(map[0])):
-        col = ''
-        for row in map:
-            col += row[i]
-        L.append(col[::-1])
-    return '\n'.join(L)
-
 def partA(input: str):
-    map = tiltMap(input)
+    map = transpose(input, tilt=True)
     for _ in range(3):
-        map = transpose(map)
+        map = transpose(map, tilt=False)
     return getLoadMap(map)
+
+def cycle(map: str) -> str:
+    map = transpose(map, tilt=True)
+    map = transpose(map, tilt=True)
+    map = transpose(map, tilt=True)
+    return transpose(map, tilt=True)
 
 def partB(input: str):
     map = input
@@ -60,10 +44,9 @@ def partB(input: str):
             break
         map_to_index[map] = i
         index_to_map[i] = map
-        # print(f'{i}\n'+next+'\n')
         i += 1
-    print(len(map_to_index)-map_to_index[map])
     return getLoadMap(index_to_map[map_to_index[map]+(1_000_000_000-map_to_index[map])%(len(map_to_index)-map_to_index[map])])
+
 
 if __name__ == '__main__':
     import sys
