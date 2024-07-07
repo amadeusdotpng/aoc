5283016c88fe9b93197075a6ece1a14f0e8f1793
amadeusdotpng <amadeus8182@gmail.com>
2024-07-07 16:18:44 -0500

    2023 day 12 part 2 solution
    oh my god fuck yes i'm unstoppable

--- y23/d12.py
+++ y23/d12.py
@@ -1,8 +1,10 @@
 from collections import deque
-from typing import Dict, List
+from functools import cache
+from typing import List, Tuple
+
 
 def verify(map: str, counts: List):
-    groups = deque([c for c in map.split('.') if c])
+    groups = deque([c for c in map.split(".") if c])
     if len(counts) != len(groups):
         return None
     for count in counts:
@@ -11,40 +13,83 @@ def verify(map: str, counts: List):
             return None
     return map
 
+
 def partA(input: str):
     input = input.splitlines()
     sum = 0
     for p, line in enumerate(input):
-        map, counts = line.split()
-        counts = [int(c) for c in counts.split(',')]
-        n_unknown = map.count('?')
+        record, counts = line.split()
+        counts = [int(c) for c in counts.split(",")]
+        n_unknown = record.count("?")
         arrangements = set()
-        print(f'{p+1} out of 1000', end='\r')
+        print(f"{p+1} out of 1000", end="\r")
         for n in range(2**n_unknown):
-            b = deque(c for c in f'{n:0>{n_unknown}b}'.replace('0', '.').replace('1', '#'))
-            nmap = ''
-            for c in map:
-                if c == '?':
-                    nmap += b.popleft()
+            b = [c for c in f"{n:0>{n_unknown}b}".replace("0", ".").replace("1", "#")]
+            nrecord = ""
+            for c in record:
+                if c == "?":
+                    nrecord += b.popleft()
                 else:
-                    nmap += c
-            if (m := verify(nmap, counts)):
+                    nrecord += c
+            if m := verify(nrecord, counts):
                 arrangements.add(m)
         sum += len(arrangements)
     print()
     return sum
 
 def partB(input: str):
+    N = 5
     input = input.splitlines()
     sum = 0
-    
+
     for p, line in enumerate(input):
-        map, counts = line.split()
-        # counts = [int(c) for 
-if __name__ == '__main__':
+        record, counts = line.split()
+
+        record = "?".join(record for _ in range(N))
+        counts = tuple(int(c) for _ in range(N) for c in counts.split(","))
+        count, counts = counts[0], counts[1:]
+
+        print(f"{p+1} out of 1000", end="\r")
+        res = dp(record, counts, count, False)
+        sum += res
+    print()
+    return sum
+
+@cache
+def dp(record: str, counts: Tuple[int], count: int, started: bool):
+    char, record = record[0], record[1:]
+    match (char, count):
+        case ('#', 0): return 0
+        case ('#', _): return (
+                0 if len(record) == 0 and count != 1 else
+                1 if len(record) == 0 and count == 1 and len(counts) == 0 else
+                0 if len(record) == 0 and count == 1 and len(counts) != 0 else
+                dp(record, counts, count - 1, True)
+        )
+
+        case ('.', 0): return (
+            1 if len(counts) == 0 and len(record) == 0 else
+            0 if len(counts) >  0 and len(record) == 0 else
+            dp(record, counts, count, False) if len(counts) == 0 and len(record) > 0 else
+            dp(record, counts[1:], counts[0], False)
+        )
+        case ('.', _): return (
+            0 if started or len(record) == 0 else
+            dp(record, counts, count, False)
+        )
+
+        case ('?', _): return (
+            dp('.'+record, counts, count, started)
+          + dp('#'+record, counts, count, started)
+        )
+
+def map_replace(S: str, R: str, i: int):
+    return S[:i] + R + S[i + 1 :]
+
+if __name__ == "__main__":
     import sys
 
-    infile = sys.argv[1] if len(sys.argv) > 1 else 'd12.in'
+    infile = sys.argv[1] if len(sys.argv) > 1 else "d12.in"
     inp = open(infile).read().strip()
-    print(f'A: {partA(inp[::])}')
-    print(f'B: {None}')
+    # print(f'A: {partA(inp[::])}')
+    print(f"B: {partB(inp[::])}")
