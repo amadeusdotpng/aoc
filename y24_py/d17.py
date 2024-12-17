93d1a2f93cf3c995159bee05d9367baa2c518105
amadeusdotpng <amadeus8182@gmail.com>
2024-12-17 04:44:20 -0600

    2024 day 17 solution fix

--- y24_py/d17.py
+++ y24_py/d17.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-
 def run_program(reg, program):
     inst_pointer = 0
     out = []
@@ -8,33 +7,23 @@ def run_program(reg, program):
         opcode = program[inst_pointer]
         opinpt = program[inst_pointer+1]
         assert opinpt != 7
+
         combo = reg.get(opinpt, opinpt)
         liter = opinpt
 
-        if opcode == 0:
-            reg[4] = reg[4] >> combo
+        match opcode:
+            case 0: reg[4] = reg[4] >> combo
+            case 1: reg[5] = reg[5] ^ liter
+            case 2: reg[5] = combo % 8
 
-        elif opcode == 1:
-            reg[5] = reg[5] ^ liter
-
-        elif opcode == 2:
-            reg[5] = combo % 8
-
-        elif opcode == 3:
-            if reg[4] != 0:
+            case 3 if reg[4] != 0: 
                 inst_pointer = liter
                 continue
-        elif opcode == 4:
-            reg[5] = reg[5] ^ reg[6]
 
-        elif opcode == 5:
-            out.append(combo % 8)
-
-        elif opcode == 6:
-            reg[5] = reg[4] >> combo
-
-        elif opcode == 7:
-            reg[6] = reg[4] >> combo
+            case 4: reg[5] = reg[5] ^ reg[6]
+            case 5: out.append(combo % 8)
+            case 6: reg[5] = reg[4] >> combo
+            case 7: reg[6] = reg[4] >> combo
 
         inst_pointer += 2
 
@@ -52,49 +41,7 @@ def partB(inp: str):
     _, program = inp.split('\n\n')
     program = list(map(int, program[9:].split(',')))
 
-    Q = deque([(n, 1) for n in range(2**10)])
-
-    #print(''.join(str(k) for k in program))
-    while Q:
-        n, idx = Q.popleft()
-        m = program[:idx]
-
-        reg = {4: n, 5: 0, 6: 0}
-        res = run_program(reg, program)
-
-        #print(''.join(str(res[i]) if i < len(res) else 'X' for i in range(16)), f'{n:0>20}', idx, end='\r')
-
-        if res[:idx] != m:
-            continue
-
-        if res == program:
-            #print(''.join(str(res[i]) if i < len(res) else 'X' for i in range(16)), f'{n:0>20}', idx, end='\r')
-            #print()
-            return n
-
-        if idx+1 >= len(program):
-            continue
-
-
-        for k in range(1,2**4):
-            e = n | k << n.bit_length()
-            '''
-            print(f'{k:0>3b} {n:b}')
-            print(f'{e:b}')
-            print()
-            '''
-            Q.append((e, idx+1))
-
-def partC(inp: str):
-    _, program = inp.split('\n\n')
-    program = list(map(int, program[9:].split(',')))
-
-    reg = {4: 0, 5: 0, 6: 0}
-    [rest] = run_program(reg, program)
-
-    # from time import sleep
-    # print(''.join(str(k) for k in program))
-    Q = deque([(n, rest) for n in range(8)])
+    Q = deque([(n, 0) for n in range(8)])
     while Q:
         n, rest = Q.popleft()
         m = n | (rest << 3)
@@ -102,18 +49,11 @@ def partC(inp: str):
 
         res = run_program(reg, program)
 
-        # print(f'{''.join(str(s) for s in res):X>{len(program)}}', f'{m:0>15}', end='\r')
-        # sleep(0.050)
-
         if res != program[-len(res):]:
             continue
 
         if res == program:
-            # print()
             return m
-
-        if len(res) > len(program):
-            continue
 
         for k in range(8):
             Q.append((k, m))
@@ -124,4 +64,4 @@ if __name__ == '__main__':
     infile = sys.argv[1] if len(sys.argv) > 1 else 'd17.in'
     inp = open(infile).read().strip()
     print(f'A: {partA(inp[::])}')
-    print(f'B: {partC(inp[::])}')
+    print(f'B: {partB(inp[::])}')
