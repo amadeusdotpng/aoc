617c1b9d77c65af246bb5a408e414adef68b7bd3
amadeusdotpng <amadeus8182@gmail.com>
2025-12-07 14:46:36 -0600

    2025 day 7 better solution

--- y25_py/d07.py
+++ y25_py/d07.py
@@ -1,78 +1,36 @@
 from collections import defaultdict
 
-def partA(inp: str):
+def solve(inp: str):
+    M = [(p % 2, [c for c in line.strip('.')[::2]]) for p, line in enumerate(inp.splitlines()[2::2])]
+
     C = 0
-    S = defaultdict(set)
-    B = defaultdict(set)
+    P_0 = defaultdict(int, {(0, 0): 1})
+    for (k, (e, ms)) in enumerate(M):
+        P_1 = defaultdict(int)
+        P = [(i, k-i) for i in range(k+1)]
 
-    lines = inp.splitlines()
-    L = len(lines)
-    for r, line in enumerate(lines):
-        for c, cr in enumerate(line):
-            if cr == 'S': S[r].add(c)
-            if cr == '^': B[r].add(c)
-
-
-    for r in range(L):
-        if r not in B: continue
+        for (a, b), m in zip(P, ms):
+            if m == '^':
+                P_1[(a+1, b)] += P_0[(a, b)]
+                P_1[(a, b+1)] += P_0[(a, b)]
+                C += P_0[(a, b)] != 0
+            if m == '.':
+                P_1[(a+1, b+1)] += P_0[(a, b)]
         
-        for b in B[r]:
-            r_b = r - 1
-            while True:
-                if r_b < 0: break
-                if r_b in B and b in B[r_b]: break
+        for (a, b), v in P_0.items():
+            if (a+b)%2 == e: continue
+            P_1[(a, b)] += v
 
-                if r_b in S and b in S[r_b]:
-                    S[r].add(b - 1)
-                    S[r].add(b + 1)
-                    C += 1
-                    break
+        P_0 = P_1
 
-                r_b -= 1
+    return C, sum(P_0.values())
 
-    return C
+def partA(inp: str):
+    return solve(inp)[0]
 
 
 def partB(inp: str):
-    B = defaultdict(set)
-    P = defaultdict(set)
-
-    C_0 = defaultdict(int)
-
-    lines = inp.splitlines()
-    H = len(lines)
-    for r, line in enumerate(lines):
-        for c, cr in enumerate(line):
-            if cr == 'S':
-                B[r].add(c)
-                C_0[c] = 1
-            if cr == '^': P[r].add(c)
-
-    for r in range(H):
-        if r not in P: continue
-
-        C_1 = defaultdict(int)
-        for p in P[r]:
-            r_p = r - 1
-            while True:
-                if r_p < 0: break
-                if r_p in P and p in P[r_p]: break
-
-                if r_p in B and p in B[r_p]:
-                    B[r].add(p - 1)
-                    B[r].add(p + 1)
-                    C_1[p-1] += C_0[p]
-                    C_1[p+1] += C_0[p]
-                    break
-
-                r_p -= 1
-
-        for (c_b, c) in C_0.items():
-            C_1[c_b] += 0 if c_b in P[r] else c
-
-        C_0 = C_1
-
-    return sum(C_0.values())
+    return solve(inp)[1]
 
 if __name__ == '__main__':
     import sys
